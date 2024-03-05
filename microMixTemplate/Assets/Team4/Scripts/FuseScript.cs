9068f16c1cc67202a385f9ba437d5e701f6b5269
TheSuperfon <astefanadon@gmail.com>
2024-03-05 15:04:03 -0500

    Bomb fuse prototype task finished
    bomb and fuse are made but not knife, cutting or game states

---
+++ microMixTemplate/Assets/Team4/Scripts/FuseScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseScript : MonoBehaviour
+{
+    public Transform SparkLocation;
+    public Transform Endfuse;
+    bool activecoroutine;
+    LineRenderer Fuse;
+    public float fusetime;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fuse = GetComponent<LineRenderer>();
+        Fuse.positionCount = 2;
+        Fuse.SetPosition(0, new Vector2(SparkLocation.position.x, SparkLocation.position.y));
+        Fuse.SetPosition(1, new Vector2(Endfuse.position.x, Endfuse.position.y));
+        activecoroutine = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activecoroutine == true)
+        {
+            StartCoroutine(LitBomb());
+
+        }
+        
+        
+        
+        
+    }
+
+    public IEnumerator LitBomb()
+    {
+        activecoroutine = false;
+        
+        if (SparkLocation.position.x >= Endfuse.position.x)
+        {
+            SparkLocation.Translate (-fusetime, 0, 0);
+            //Debug.Log("test");
+            Fuse.SetPosition(0, new Vector2(SparkLocation.position.x, SparkLocation.position.y));
+            yield return new WaitForSeconds(0.05f);
+        }
+        
+        activecoroutine = true;
+
+    }
+
+
+    void Newspark()
+    {
+        
+    }
+
+
+}
