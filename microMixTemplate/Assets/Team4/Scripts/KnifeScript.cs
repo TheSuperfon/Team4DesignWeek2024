1e37663efac4096afd56fd002c8b44d0cb6f5ee9
kaziau <kaziau@sheridancollege.ca>
2024-03-05 23:20:13 -0500

    Added art assets
    Added fox and roadrunner rough anim sprites, and the butterknife (not implemented) and the spark animation (implemented).

--- microMixTemplate/Assets/Team4/Scripts/KnifeScript.cs
+++ microMixTemplate/Assets/Team4/Scripts/KnifeScript.cs
@@ -27,7 +27,11 @@ public class KnifeScript : MicrogameInputEvents
     // Update is called once per frame
     void Update()
     {
+        // if joystick input is detected, but no button press:
         moveknife();
+
+        // if button press is ever detected:
+        // ActivateCutMode;
 
 
 
@@ -46,6 +50,20 @@ public class KnifeScript : MicrogameInputEvents
         transform.position = NewKnifeLocation;
     }
 
+    // Function to activate 'cut' mode: (pseudocode for now)
+    void ActivateCutMode()
+    {
+        // cut mode is true. (not sure if this bool check needs to be here or in update)
+        // stop horizontal movement
+        // detect up/down movement only
+        // if player moves up/down a certain amount, count it as 1 cut
+        // else do nothing (do not add a cut)
+        // if the cut count ever reaches [x] amount, set a cutSuccess variable as true, and show win state
+        // on button release exit this function by setting cut mode to false, or by using return?
 
+        // we didn't discuss it, but there should probably also be a function that makes players unable
+        // to continue cutting and the game counting it if that part of the rope is now burned/gone. 
+
+    }
 
 }
